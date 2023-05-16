0f6658cf9b5dc57275d06dc58d835a49b34e3514
Yi Dai <yidai1996@gmail.com>
2023-05-16 16:54:24 -0400

    Some updates

--- BicultureStability.jl
+++ BicultureStability.jl
@@ -1,6 +1,7 @@
 using LinearAlgebra
 function JMatrix4D(mumax_Av, kd_Av, Ks_Av, Ys_Av, Y_NC_Av, C_N, X_Av, mumax_Se, kd_Se, Ks_Se, Ys_Se, Y_CN_Se, C_C, X_Se)
     J = zeros(4,4)
+    # Only Monod model
     mu_Av = mumax_Av*C_C/(Ks_Av+C_C)
     mu_Se = mumax_Se*C_N/(Ks_Se+C_N)
     J[1,1] = mu_Av - kd_Av
@@ -19,8 +20,26 @@ function JMatrix4D(mumax_Av, kd_Av, Ks_Av, Ys_Av, Y_NC_Av, C_N, X_Av, mumax_Se, 
     J[4,2] = - mu_Se / Ys_Se
     J[4,3] = Y_NC_Av * X_Av / Ys_Av *mumax_Av * Ks_Av /(Ks_Av+C_C)^2
     J[4,4] = - X_Se / Ys_Se * mumax_Se * Ks_Se /(Ks_Se+C_N)^2
-    # println(J)
 
+    # Monod + L-P model
+    # mu_Av = mumax_Av*C_C/(Ks_Av+C_C)
+    # mu_Se = mumax_Se*C_N/(Ks_Se+C_N)
+    # J[1,1] = mu_Av - kd_Av
+    # J[1,2] = 0
+    # J[1,3] = mu_Av * Ks_Av / (Ks_Av+C_C)^2 * X_Av
+    # J[1,4] = 0
+    # J[2,1] = 0
+    # J[2,2] = mu_Se - kd_Se
+    # J[2,3] = 0
+    # J[2,4] = mu_Se * Ks_Se /(Ks_Se+C_N)^2 * X_Se
+    # J[3,1] = - mu_Av / Ys_Av
+    # J[3,2] = Y_CN_Se * (mu_Se - kd_Se) + beta_C
+    # J[3,3] = - X_Av / Ys_Av * mumax_Av * Ks_Av / (Ks_Av + C_C)^2
+    # J[3,4] = Y_CN_Se * X_Se *mumax_Se * Ks_Se / (Ks_Se + C_N)^2
+    # J[4,1] = Y_NC_Av * (mu_Av -kd_Av) + beta_N
+    # J[4,2] = - mu_Se / Ys_Se
+    # J[4,3] = Y_NC_Av * X_Av *mumax_Av * Ks_Av / (Ks_Av + C_C)^2
+    # J[4,4] = - X_Se / Ys_Se * mumax_Se * Ks_Se / (Ks_Se + C_N)^2
     return J
 end
 
@@ -30,7 +49,7 @@ function StabilityDiagramDataGenerator4D(test)
     # println(lambda)
     vec = eigvecs(A)
     # print(size(vec))
-    return lambda, vec
+    return lambda, vec, A
 end
 
 # function JMatrix2D(mumax_Av, kd_Av, Ks_Av, Ys_Av, Y_NC_Av, C_N, X_Av, mumax_Se, kd_Se, Ks_Se, Ys_Se, Y_CN_Se, C_C, X_Se)
@@ -60,31 +79,40 @@ end
 #     return lambda, vec
 # end
 
-include("Monoculture_producer_TriCulture_Chemostat_Monod.jl")
 
 
 
-mumax_Av = 0.1
-kd_Av = 0.1
-Ks_Av = 0.3
+
+mumax_Av = 0.5
+kd_Av = 0.05*mumax_Av # 5% of mumax_Av
+Ks_Av = 5*17.03*10^(-3) # 5mM of ammonia
 Ys_Av = 1
-Y_NC_Av = 2
-mumax_Se = 0.2
-kd_Se = 0.1
-Ks_Se = 0.001
-Ys_Se = 5  
-Y_CN_Se = 0.5
+Y_NC_Av = 2 # alpha_N
+mumax_Se = 0.1
+kd_Se = 0.05*mumax_Se
+# Ks_Se = 5*342.3*10^(-3) # 5mM of sucrose
+Ks_Se = 0.344 # 1.5 1.3 1.1 0.9 0.7 0.5 0.3 0.1
+Ys_Se = 1  
+Y_CN_Se = 0.5 # alpha_C
+beta_C = 0 # 0 # 0.5
+beta_N = 0 # 0 # 0.5
+
+# ParameterS = [mumax_Se, Ks_Se, Ys_Se, Y_CN_Se, 0];
+# ParameterA = [mumax_Av, Ks_Av, Ys_Av, Y_NC_Av, 0];
+# C_N = 2
+# C_C = 0
+# CocultureGrowth(ParameterS, ParameterA, C_N, C_C; filename = "C_$(@sprintf("%.2f",C_C))_N__$(@sprintf("%.2f",C_N))")
 
 # X_Ax = X_Se = 0
 # C_N_list = collect(0:0.1:4);
-C_N_list = collect(0:1:100);
+C_N_list = LinRange(0,100,101);
 C_C_list = C_N_list;
 X_Se = 0
 X_Av = 0
 
 Stability = zeros(length(C_C_list)*length(C_N_list),3);
 println(length(C_C_list)*length(C_N_list))
-
+include("Monoculture_producer_TriCulture_Chemostat_Monod.jl")
 function StabilityAnalysis2D_Nutrient(C_N_list, C_C_list)
     eigenvalue = zeros(length(C_C_list)*length(C_N_list), 4);
     eigenvector = zeros(length(C_C_list)*length(C_N_list), 4, 4);
@@ -114,9 +142,11 @@ function StabilityAnalysis2D_Nutrient(C_N_list, C_C_list)
             else Stability[count,:] = [C_C C_N 1]
             end
 
-            ParameterS = [mumax_Se, Ks_Se, Ys_Se, Y_CN_Se, 0];
-            ParameterA = [mumax_Av, Ks_Av, Ys_Av, Y_NC_Av, 0];
-            CocultureGrowth(ParameterS, ParameterA, C_N, C_C; filename = "C_$(@sprintf("%.2f",C_C))_N__$(@sprintf("%.2f",C_N))")
+            # ParameterS = [mumax_Se, Ks_Se, Ys_Se, Y_CN_Se, 0.5]; # the last element is ms
+            # ParameterA = [mumax_Av, Ks_Av, Ys_Av, Y_NC_Av, 0.5];
+            ParameterS = [mumax_Se, Ks_Se, Ys_Se, Y_CN_Se, beta_C];
+            ParameterA = [mumax_Av, Ks_Av, Ys_Av, Y_NC_Av, beta_N];
+            # CocultureGrowth(ParameterS, ParameterA, C_N, C_C; filename = "C_$(@sprintf("%.2f",C_C))_N__$(@sprintf("%.2f",C_N))")
         end
     end
 
@@ -124,6 +154,10 @@ function StabilityAnalysis2D_Nutrient(C_N_list, C_C_list)
 end
 
 A,B,C = StabilityAnalysis2D_Nutrient(C_N_list, C_C_list)
+using DataFrames, CSV
+df = DataFrame(Sucrose = C[:,1], Ammonia = C[:,2], Stability = C[:,3], Eigenvalue1 = A[:,1], Eigenvalue2 = A[:,2], Eigenvalue3 = A[:,3], Eigenvalue4 = A[:,4], Eigenvector1_1 = B[:,1,1], Eigenvector1_2 = B[:,1,2], Eigenvector1_3 = B[:,1,3], Eigenvector1_4 = B[:,1,4], 
+    Eigenvector2_1 = B[:,2,1], Eigenvector2_2 = B[:,2,2], Eigenvector2_3 = B[:,2,3], Eigenvector2_4 = B[:,2,4], Eigenvector3_1 = B[:,3,1], Eigenvector3_2 = B[:,3,2], Eigenvector3_3 = B[:,3,3], Eigenvector3_4 = B[:,3,4], Eigenvector4_1 = B[:,4,1], Eigenvector4_2 = B[:,4,2], Eigenvector4_3 = B[:,4,3], Eigenvector4_4 = B[:,4,4])
+CSV.write("C:\\Users\\yid\\TemporaryResearchDataStorage\\doe22_tspan_200\\StabilityAnalysisResults\\LinearStabilityRegionsEigenValueVectors.csv",df)
 
 index_unstable = findall(x-> x==3, C[:,3])
 index_lyapunov_stable = findall(x-> x==2, C[:,3])
@@ -149,7 +183,16 @@ show(stdout, "text/plain", C[index_stable,:])
 show(stdout, "text/plain", C[index_lyapunov_stable,:])
 show(stdout, "text/plain", C[index_unstable,:])
 
+New_data_0 = C[index_unstable,1:2]
+New_data_1 = C[index_lyapunov_stable,1:2]
+New_data_2 = C[index_stable,1:2]
+
 using Plots
+scatter(New_data_0[:,1], New_data_0[:,2], xlabel = "C(Sucrose)", ylabel = "C(Ammonia)", label = "Unstable", markersize = 4, size = [1000,1000], markercolor = "Deep Sky Blue", markerstrokewidth = 0, legendfontsize=18, xtickfont = 18, ytickfont = 18, xguidefont = 18, yguidefont = 18)
+scatter!(New_data_1[:,1], New_data_1[:,2], label = "Lyapunov Stable", markersize = 4,  markercolor = "Light Pink", markerstrokewidth = 0)
+scatter!(New_data_2[:,1], New_data_2[:,2], label = "Stable", markersize = 4, markercolor = "Dark Magenta", markerstrokewidth = 0)
+Plots.savefig("C:\\Users\\yid\\TemporaryResearchDataStorage\\doe21_tspan_200\\StabilityAnalysisResults\\LinearAnaylsis.pdf")
+
 rectangle(w, h, x, y) = Shape(x .+ [0,w,w,0], y .+ [0,0,h,h])
 Plots.plot(rectangle(100, 100, 0, 0), xlabel="C(Sucrose)", ylabel="C(Ammonia)", label="Unstable")
 # Plots.plot(rectangle(100, 100, 0, 0), xlabel="C(Sucrose)", ylabel="C(Ammonia)", label="Lyapunov")
@@ -175,13 +218,13 @@ print(df)
 
 # show(stdout,"text/plain", C)
 
-# # test if jacobian calculation is correct
-# using ForwardDiff
-# import ForwardDiff.jacobian
-# f(Av,Se,C,N) = [(mumax_Av*C/(Ks_Av+C) - kd_Av)*Av, 
-#                 (mumax_Se*N/(Ks_Se+N) - kd_Se)*Se, 
-#                 Y_CN_Se/Ys_Se*mumax_Se*N/(Ks_Se+N)*Se - mumax_Av*Ys_Av*C/(Ks_Av+C)*Av,
-#                 Y_NC_Av/Ys_Av*mumax_Av*C/(Ks_Av+C)*Av - mumax_Se/Ys_Se*N/(Ks_Se+N)*Se]
-# x0 = [0.0, 0.0, 0.1, 0.1];
-# J0 = jacobian(x -> f(x...) ,x0)
+# test if jacobian calculation is correct
+using ForwardDiff
+import ForwardDiff.jacobian
+f(Av,Se,C,N) = [(mumax_Av*C/(Ks_Av+C) - kd_Av)*Av, 
+                (mumax_Se*N/(Ks_Se+N) - kd_Se)*Se, 
+                Y_CN_Se*(mumax_Se*N/(Ks_Se+N) - kd_Se)*Se + beta_C*Se - mumax_Av*C/(Ks_Av+C)*Av/Ys_Av,
+                Y_NC_Av*(mumax_Av*C/(Ks_Av+C) - kd_Av)*Av + beta_N*Av - mumax_Se/Ys_Se*N/(Ks_Se+N)*Se]
+x0 = [0.0, 0.0, 0.004715, 0.111833];
+J0 = jacobian(x -> f(x...) ,x0)
 # show(stdout, "text/plain", Real_lyapunov_stable)
--- FindStabilityRegions.jl
+++ FindStabilityRegions.jl
@@ -1,32 +1,206 @@
 using XLSX, DataFrames, CSV
 
 # Distinguish stability by checking the last two points in all growth curves
-out_dir = "C:\\Users\\yid\\TemporaryResearchDataStorage\\doe3_tspan_200\\excel\\"
+out_dir = "C:\\Users\\yid\\TemporaryResearchDataStorage\\doe21_tspan_200\\excel\\"
 index_files = readdir(out_dir);
 # build a dataframe for C/N and stability (growth curve regions)
-df = DataFrame([Int64[], Int64[], Int64[]], ["C(Sucrose)","Ammonia","Stability"])
+df1 = DataFrame([Float64[], Float64[], Float64[]], ["Sucrose","Ammonia","Stability"])
+df2 = DataFrame([Float64[], Float64[], Float64[]], ["Sucrose","Ammonia","Stability"])
+count = 0
 for i in eachindex(index_files)
+    count += 1
     # search all excel files
     # find the last two points of biomass concentration
     ex = XLSX.readxlsx(out_dir * index_files[i])
     sh = ex["Sheet1"]
     num_rows, num_cols = size(sh[:])
+    t = sh["A2:A"*string(num_rows)]
+    t_checkpoint_row = Tuple(findall(x -> x>=50, t)[1])[1]
+    Av = sh["B2:B"*string(num_rows)]
+    N = sh["E2:E"*string(num_rows)]
+    C = sh["D2:D"*string(num_rows)]
     Av1 = sh["B" * string(num_rows-1)]
     Av2 = sh["B" * string(num_rows)]
     Se1 = sh["C" * string(num_rows-1)]
-    Se2 = sh["B" * string(num_rows)]
-    println("Av=",[Av1 Av2])
-    println("Se=",[Se1 Se2])
+    Se2 = sh["C" * string(num_rows)]
+    N_start_1 = sh["E2"]
+    N_start_2 = sh["E3"]
+    N_end = N[end]
+    C_end = C[end]
+
+    # println("Av=",[Av1 Av2])
+    # println("Se=",[Se1 Se2])
     # Check the stability
-    if num_rows<=3
-        # the stable region in linear stability analysis
-        push!(df,[sh["D2"], sh["E2"], 0])
-    elseif (Av1-Av2 <= 10^(-5)) & (Se2-Se1 <= 10^(-5))
-        # going to exponentially growth
-        push!(df,[sh["D2"], sh["E2"], 1])
-    else push!(df,[sh["D2"], sh["E2"], 2])
+    check_exponential = (Av2-Av1)/(t[end]-t[end-1])
+    println(check_exponential)
+    if check_exponential > 1
+        # going to exponential growth
+        push!(df1,[sh["D2"], sh["E2"], 0])
+    elseif (Av[t_checkpoint_row]-Av[t_checkpoint_row-1])/(t[t_checkpoint_row]-t[t_checkpoint_row-1])<0
+        # has a bump for Av growth
+        push!(df1,[sh["D2"], sh["E2"], 1])
+    else # no bumps for Av growth and going to stationary stage
+        push!(df1,[sh["D2"], sh["E2"], 2])
     end
+
+    # For only Monod model
+    if N_end-N[end-1]<0.001 
+        push!(df2,[sh["D2"], sh["E2"], 0])
+        
+    else # not stable, C_N doesn't go to the stable point
+        push!(df2,[sh["D2"], sh["E2"], 1])
+    end
+
+    # # For monod-LP model
+    # if (N_end-N[end-1])/(t[end]-t[end-1]) > 1
+    #     if C_end < 0.03
+    #         # C is stable 
+    #         push!(df2,[sh["D2"], sh["E2"], 0])
+    #     else # c NOT TOUCH THE STABLE FINAL STATE
+    #         println(C_end)
+    #         push!(df2,[sh["D2"], sh["E2"], 1])
+    #     end
+    # else # N is not increasing exponentially
+    #     push!(df2,[sh["D2"], sh["E2"], 2])
+    # end
     println(i)
 end
-println(df)
-CSV.write("C:\\Users\\yid\\TemporaryResearchDataStorage\\doe3_tspan_200\\StabilityAnalysisResults\\StabilityRegions.csv",df)
+println(count)
+println(df1)
+println(df2)
+CSV.write("C:\\Users\\yid\\TemporaryResearchDataStorage\\doe21_tspan_200\\StabilityAnalysisResults\\StabilityRegionsMicrobial.csv",df1)
+CSV.write("C:\\Users\\yid\\TemporaryResearchDataStorage\\doe21_tspan_200\\StabilityAnalysisResults\\StabilityRegionsNutrient.csv",df2)
+
+
+# Plot the stability regions
+using Plots
+# using StatsPlots
+# read data from CSV file by making a dataframe
+df_nutrient = CSV.read("C:\\Users\\yid\\TemporaryResearchDataStorage\\doe21_tspan_200\\StabilityAnalysisResults\\StabilityRegionsNutrient.csv",DataFrame)
+df_microbial = CSV.read("C:\\Users\\yid\\TemporaryResearchDataStorage\\doe21_tspan_200\\StabilityAnalysisResults\\StabilityRegionsMicrobial.csv",DataFrame)
+count_row = nrow(df_nutrient);
+# count_feature = ncol(df_all) - 2
+df1 = Matrix(df_microbial);
+df2 = Matrix(df_nutrient);
+A1 = df1[:,1:2];
+Microbial_Region = df1[:,3];
+A2 = df2[:,1:2];
+Nutrient_Region = df2[:,3];
+# println(Region)
+count_total = 0;
+count_0_region = 0;
+count_1_region = 0;
+count_2_region = 0;
+New_data_0 = zeros(1,2)
+New_data_1 = zeros(1,2)
+New_data_2 = zeros(1,2)
+
+for i in eachindex(Microbial_Region)
+    count_total += 1
+    # Go through all test data
+    if Microbial_Region[i] == 0
+        # println("Find parallel")
+        count_0_region += 1
+        # println(A[i,:])
+        if count_0_region == 1
+            New_data_0[:] = transpose(A1[i,:])
+        else 
+            New_data_0 = vcat(New_data_0, transpose(A1[i,:]))
+        end
+
+    elseif Microbial_Region[i] == 1
+        count_1_region += 1
+        if count_1_region == 1
+            New_data_1[:] = transpose(A1[i,:])
+        else 
+            New_data_1 = vcat(New_data_1, transpose(A1[i,:]))
+        end
+
+    else Microbial_Region[i] == 2
+        count_2_region += 1
+        if count_2_region == 1
+            New_data_2[:] = transpose(A1[i,:])
+        else 
+            New_data_2 = vcat(New_data_2, transpose(A1[i,:]))
+        end
+    end
+end
+
+scatter(New_data_0[:,1], New_data_0[:,2], xlabel = "C(Sucrose)", ylabel = "C(Ammonia)", label = "Exponential growth", markersize = 4, size = [1000,1000], markercolor = "Deep Sky Blue", markerstrokewidth = 0, legendfontsize=18, xtickfont = 18, ytickfont = 18, xguidefont = 18, yguidefont = 18)
+scatter!(New_data_1[:,1], New_data_1[:,2], label = "With A Bump", markersize = 4,  markercolor = "Light Pink", markerstrokewidth = 0)
+scatter!(New_data_2[:,1], New_data_2[:,2], label = "Without Bumps", markersize = 4, markercolor = "Dark Magenta", markerstrokewidth = 0)
+
+Plots.savefig("C:\\Users\\yid\\TemporaryResearchDataStorage\\doe21_tspan_200\\StabilityAnalysisResults\\StabilityRegionsMicrobial.pdf")
+
+count_total = 0;
+count_0_region = 0;
+count_1_region = 0;
+count_2_region = 0;
+New_data_0 = zeros(1,2)
+New_data_1 = zeros(1,2)
+New_data_2 = zeros(1,2)
+for i in eachindex(Nutrient_Region)
+    count_total += 1
+    # Go through all test data
+    if Nutrient_Region[i] == 0
+        # println("Find parallel")
+        count_0_region += 1
+        # println(A[i,:])
+        if count_0_region == 1
+            New_data_0[:] = transpose(A2[i,:])
+        else 
+            New_data_0 = vcat(New_data_0, transpose(A2[i,:]))
+        end
+    
+    # Monod L-P model
+    elseif Nutrient_Region[i] == 2
+        # println("Find parallel")
+        count_2_region += 1
+        # println(A[i,:])
+        if count_2_region == 1
+            New_data_2[:] = transpose(A2[i,:])
+        else 
+            New_data_2 = vcat(New_data_2, transpose(A2[i,:]))
+        end
+
+    else 
+        count_1_region += 1
+        if count_1_region == 1
+            New_data_1[:] = transpose(A2[i,:])
+        else 
+            New_data_1 = vcat(New_data_1, transpose(A2[i,:]))
+        end
+    end
+end
+println(count_1_region)
+scatter(New_data_0[:,1], New_data_0[:,2], xlabel = "C(Sucrose)", ylabel = "C(Ammonia)", label = "C Stable State", markersize = 4, size = [1000,1000], markercolor = "Deep Sky Blue", markerstrokewidth = 0, legendfontsize=18, xtickfont = 18, ytickfont = 18, xguidefont = 18, yguidefont = 18, legend=:topright)
+scatter!(New_data_1[:,1], New_data_1[:,2], label = "C Not Touch Stable State", markersize = 4,  markercolor = "Light Pink", markerstrokewidth = 0)
+scatter!(New_data_2[:,1], New_data_2[:,2], label = "N decreasing", markersize = 2, markercolor = "Dark Magenta", markerstrokewidth = 0)
+
+Plots.savefig("C:\\Users\\yid\\TemporaryResearchDataStorage\\doe21_tspan_200\\StabilityAnalysisResults\\StabilityRegionsNutrient.pdf")
+
+# using PlotlyJS
+# plot([
+#     scatter(x = New_data_0[:,1], y = New_data_0[:,2], fill = "tozeroy", mode = "none"),
+#     scatter(x = New_data_1[:,1], y = New_data_1[:,2], fill = "tonexty", mode = "none"),
+#     scatter(x = New_data_2[:,1], y = New_data_2[:,2], fill = "tonexty", mode = "none")
+# ])
+# plot([
+#     scatter(x = New_data_0[:,1], y = New_data_0[:,2]),
+#     scatter(x = New_data_1[:,1], y = New_data_1[:,2]),
+#     scatter(x = New_data_2[:,1], y = New_data_2[:,2])
+# ])
+
+# plot([
+#     scatter(x=1:4, y=[0, 2, 3, 5], fill="tozeroy"), # fill down to xaxis
+#     scatter(x=1:4, y=[3, 5, 1, 7], fill="tonexty"), # fill to trace0 y
+# ])
+using VegaLite
+df_all |>
+stack |>
+
+using CategoricalArrays
+cv = categorical(df_all[!,"Stability"])
+df_all[!,"RegionCategories"] = cv
+@vlplot(:area, x=:Sucrose, y={:Ammonia, stack=:zero}, color = "RegionCategories:n")
+df_all[!,:Stability] = parse.(Symbol, df_all[!, :Stability])
